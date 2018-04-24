6aa613c23d9f40fa835480ca9854d3c754b72c7a
seaseeyoul <seaseeyoul@qq.com>
2018-04-24 16:29:34 +0800

    design 任务一

--- design/js/task-one.js
+++ design/js/task-one.js
@@ -2,10 +2,6 @@ window.onload = function(){
     var button = document.querySelector('#j-button'),
         box = document.querySelector('#j-box');
     button.onclick = function(){
-        toggle();
-    }
-    function toggle(){
         box.classList.toggle('active')
     }
-
 }
